b3cfe146787dbe7e9f346a2cfc7b58f9e5a71fc7
Matheus Passos <56652203+pinpassos@users.noreply.github.com>
2021-04-27 00:13:46 -0300

    First commit

--- sequenciaCrescente.php
+++ sequenciaCrescente.php
@@ -2,26 +2,27 @@
 
 function sequenciaCrescente($array) {
     if(count($array) == 1) {
-        return True;
+        return 'True 1';
     } else {
-        $flag = False;
-        for($i = 0; $i <= count($array); $i++) {
+        $flag = 'False';
+        for($i = 0; $i < count($array); $i++) {
             $temp = $array;
             unset($temp[$i]);
-            for($j = 0; $j <= count($temp) - 1; $j++) {
-                if($temp[$j] < $temp[$j + 1]) {
-                    break;
+            $temp = array_values($temp);
+            $temp1 = $temp;
+            sort($temp1);
+            if($temp == $temp1) {
+                $flag = 'True';
+                for($j = 0; $j < count($temp) - 1; $j++) {
+                    if($temp[$j] == $temp[$j + 1]) {
+                        $flag = 'False';
+                    }
                 }
-                $flag = True;
+               if($flag == 'True')
+                return 'True';
             }
-        }
-        return $flag;
+        } return $flag;
     }
 }
-
-echo sequenciaCrescente([1, 3, 2, 1]);
-echo '<br>';
-echo '<br>';
-echo sequenciaCrescente([1, 3, 2]);
 
 ?>
